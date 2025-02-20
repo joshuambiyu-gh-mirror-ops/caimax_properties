fb75bf0246cb15e3170d74e2411486e22683bd31
Joshua Mbiyu <joshuambiyu02@gmail.com>
2025-02-20 09:40:16 +0300

    Replaced export default handler with export async function GET(req)

--- src/app/api/google/callback.js
+++ src/app/api/google/callback.js
@@ -1,17 +1,43 @@
-import { setCookie } from "cookies-next";
+// import { setCookie } from "cookies-next";
+// import passport from "passport";
+// import "../../../lib/passport";
+
+// export default async function handler(req, res) {
+//   passport.authenticate("google", (err, user, info) => {
+//     if (err || !user) {
+//       return res.redirect("https://caimax.co.ke/?a=auth_fail");
+//     }
+
+//     if (info?.token) {
+//       setCookie("token", info.token, { req, res, httpOnly: true });
+//     }
+
+//     return res.redirect("https://caimax.co.ke/");
+//   })(req, res);
+// }
+
+
+import { cookies } from "next/headers";
 import passport from "passport";
-import "../../../lib/passport";
+import "../../../../lib/passport"; // Adjust path if necessary
+import { NextResponse } from "next/server";
 
-export default async function handler(req, res) {
-  passport.authenticate("google", (err, user, info) => {
-    if (err || !user) {
-      return res.redirect("https://caimax.co.ke/?a=auth_fail");
-    }
+export async function GET(req) {
+  return new Promise((resolve) => {
+    passport.authenticate("google", (err, user, info) => {
+      if (err || !user) {
+        return resolve(NextResponse.redirect("https://caimax.co.ke/?a=auth_fail"));
+      }
 
-    if (info?.token) {
-      setCookie("token", info.token, { req, res, httpOnly: true });
-    }
+      if (info?.token) {
+        cookies().set("token", info.token, {
+          httpOnly: true,
+          secure: true,
+          path: "/",
+        });
+      }
 
-    return res.redirect("https://caimax.co.ke/");
-  })(req, res);
+      return resolve(NextResponse.redirect("https://caimax.co.ke/"));
+    })(req);
+  });
 }
