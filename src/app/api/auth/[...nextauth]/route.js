18c216bcdffeceac3fd8876fbe52f0267f6b46d1
Joshua Mbiyu <joshuambiyu02@gmail.com>
2025-02-20 21:25:41 +0300

    added session callback, prisma adapter authOption

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,16 +1,26 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/db";
 
 const authOptions = {
+  adapter: PrismaAdapter(db),
+  secret: process.env.AUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  // (Optional) Add callbacks, secret, database, etc.
+  callbacks: {
+    async session({ session, user }) {
+      if (session && user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
 };
 
-// The NextAuth handler is exported as both GET and POST
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
