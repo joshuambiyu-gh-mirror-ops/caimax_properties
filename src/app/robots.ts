30db904520725736cc9dc96d6fe2d99a35fef7ca
Joshua Mbiyu <joshuambiyu02@gmail.com>
2025-02-22 17:44:59 +0300

    add robots and sitemap generation with dynamic base URL configuration

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+export default async function robots(): Promise<MetadataRoute.Robots>{
+    return{
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: ["/admin", "/privacy", "/terms", "/contact"],
+            }
+        ],
+        sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`
+    }
+}
+        
