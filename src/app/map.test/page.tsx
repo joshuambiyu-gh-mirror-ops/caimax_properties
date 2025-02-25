0e5f913c4e1e80756cf1c06c07f86d5926f72892
Joshua Mbiyu <joshuambiyu02@gmail.com>
2025-02-26 01:25:57 +0300

    working map

--- src/app/map.test/page.tsx
+++ src/app/map.test/page.tsx
@@ -1,27 +1,3 @@
-// 'use client';
-// import React, { useState } from 'react';
-// import { Map } from 'react-map-gl';
-
-// export default function MapTestPage() {
-//     const [viewport, setViewport] = useState({
-//         latitude: 37.7577,
-//         longitude: -122.4376,
-//         zoom: 11
-//     });
-    
-//     return (
-//         <div style={{ width: '100%', height: '100vh' }}> {/* Add container with height */}
-//             <Map
-//                 mapStyle="mapbox://styles/mbiyu/cm7j261lv00nw01s87o2cfs3j"
-//                 mapboxAccessToken="pk.eyJ1IjoibWJpeXUiLCJhIjoiY203aXZ0cGQxMDBsdzJqc2EwdXB6ZngxciJ9.tY4trIwdOSdm1_Z0EXq-CQ" 
-//                 initialViewState={viewport}
-//                 style={{ width: '100%', height: '100%' }} // Add dimensions to Map
-//             />
-//         </div>
-//     );
-// }
-
-
 "use client";
 import { useState } from 'react';
 import Map, { ViewState } from 'react-map-gl';
@@ -44,6 +20,7 @@ export default function Home() {
         <Map
           {...viewState}
           onMove={(evt) => setViewState(evt.viewState)}
+          // mapStyle="mapbox://styles/mbiyu/cm7j261lv00nw01s87o2cfs3j"
           mapStyle="mapbox://styles/mapbox/streets-v11"
           mapboxAccessToken="pk.eyJ1IjoibWJpeXUiLCJhIjoiY203aXZ0cGQxMDBsdzJqc2EwdXB6ZngxciJ9.tY4trIwdOSdm1_Z0EXq-CQ" 
 
